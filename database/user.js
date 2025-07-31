4dae3adb1eb50e3c4ad36bf45edf1b79254fceea
jeramyleon <jeramyleon@gmail.com>
2025-07-31 11:42:45 -0400

    added user models

--- database/user.js
+++ database/user.js
@@ -3,14 +3,27 @@ const db = require("./db");
 const bcrypt = require("bcrypt");
 
 const User = db.define("user", {
-  username: {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true, 
+    primaryKey: true, 
+  },
+
+  name: {
     type: DataTypes.STRING,
     allowNull: false,
-    unique: true,
-    validate: {
-      len: [3, 20],
-    },
   },
+
+  bio: {
+    type: DataTypes.TEXT, 
+    allowNull: true
+  },
+
+  avatar_url: {
+    type: DataTypes.ENUM("image", "gif"), 
+    allowNull: true,    
+  },
+
   email: {
     type: DataTypes.STRING,
     allowNull: true,
@@ -19,15 +32,23 @@ const User = db.define("user", {
       isEmail: true,
     },
   },
+
   auth0Id: {
     type: DataTypes.STRING,
     allowNull: true,
     unique: true,
   },
-  passwordHash: {
+
+  password_hash: {
     type: DataTypes.STRING,
     allowNull: true,
+    comment: "Securely hashed password",
   },
+
+  created_at: {
+    type: DataTypes.DATE, 
+    defaultValue: DataTypes.NOW
+  }
 });
 
 // Instance method to check password
