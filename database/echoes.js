fc7abb9bf0d9b73b29bba724db60d0931bb450fb
jeramyleon <jeramyleon@gmail.com>
2025-07-31 14:17:33 -0400

    added echoes model

---
+++ database/echoes.js
@@ -0,0 +1,58 @@
+const { DataTypes } = require("sequelize");
+const db = require("./db");
+
+const Echoes = db.define("user", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true, 
+    primaryKey: true, 
+  },
+
+ sender_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+ },
+
+ type: {
+    type: DataTypes.ENUM("self", "friend", "public"),
+    allowNull: false,
+ },
+
+ text: {
+    type: DataTypes.STRING,
+    allowNull: true, 
+ },
+
+ unlock_datetime: {
+    type: DataTypes.DATE, 
+    allowNull: false
+ },
+
+ is_unlocked: {
+    type: DataTypes.VIRTUAL,
+    get() {
+        const now = new Date();
+        return now >= this.unlock_datetime; 
+    }
+ },
+
+ is_archived: {
+    type: DataTypes.BOOLEAN, 
+    defaultValue: false, 
+    allowNull: false,
+ },
+
+ show_sender_name: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false, 
+    allowNull: false, 
+ }
+
+}, {
+    tableName: 'Echoes', 
+    timestamps: true, 
+    createdAt: 'created_at', 
+    updatedAt: 'updated_at',
+});
+
+module.exports = Echoes;
