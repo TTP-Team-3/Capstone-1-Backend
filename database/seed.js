3518763729865489928d248a5b4352a6b9f1912a
jeramyleon <jeramyleon@gmail.com>
2025-08-06 11:49:49 -0400

    added seed data for echoes

--- database/seed.js
+++ database/seed.js
@@ -17,7 +17,37 @@ const seed = async () => {
 
     // Create more seed data here once you've created your models
     // Seed files are a great way to test your database schema!
-
+    const echoes = await Echoes.bulkCreate([
+      {
+        sender_id: users[0].id,
+        type: "self",
+        text: "This is Jeramy’s private echo",
+        unlock_datetime: new Date(Date.now() + 1000 * 60 * 60), // unlock in 1 hour
+        show_sender_name: true
+      },
+      {
+        sender_id: users[1].id,
+        type: "friend",
+        text: "Aiyanna’s echo to friends",
+        unlock_datetime: new Date(Date.now() - 1000 * 60 * 60), // already unlocked
+        show_sender_name: false
+      },
+      {
+        sender_id: users[2].id,
+        type: "public",
+        text: "Public echo by Emmanuel",
+        unlock_datetime: new Date(),
+        is_archived: true,
+        show_sender_name: true
+      },
+      {
+        sender_id: users[3].id,
+        type: "public",
+        text: "Olivia’s echo is for everyone!",
+        unlock_datetime: new Date(Date.now() + 1000 * 60 * 60 * 24), // unlock in 1 day
+        show_sender_name: true
+      }
+    ]);
     console.log("🌱 Seeded the database");
   } catch (error) {
     console.error("Error seeding database:", error);
