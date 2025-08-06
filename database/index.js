676401bb98d3cf61e8ef426ee6d5790895b81501
jeramyleon <jeramyleon@gmail.com>
2025-08-06 11:08:15 -0400

    added associatons

--- database/index.js
+++ database/index.js
@@ -13,62 +13,160 @@ const Tags = require('./tags');
 
 //--------Defining associations-------------
 
-// Echos <> user (sender)
-Echoes.belongsTo(User, {foreignKey: 'sender_id', as: 'sender'});
-User.hasMany(Echoes, {foreignKey: 'sender_id', as: 'sent_echoes'});
+// Echoes <> user (sender)
+Echoes.belongsTo(User, {
+  foreignKey: 'sender_id', 
+  as: 'sender', 
+  onDelete: 'CASCADE', 
+  onUpdate: 'CASCADE'
+});
+User.hasMany(Echoes, {
+  foreignKey: 'sender_id', 
+  as: 'sent_echoes', 
+  onDelete: 'CASCADE', 
+  onUpdate: 'CASCADE'
+});
 
 // Echoes <> media 
-Echoes.hasMany(Media, {foreignKey: 'echo_id'});
-Media.belongsTo(Echoes, {foreignKey: 'echo_id'});
+Echoes.hasMany(Media, {
+  foreignKey: 'echo_id', 
+  onDelete: 'CASCADE', 
+  onUpdate: 'CASCADE'
+});
+Media.belongsTo(Echoes, {
+  foreignKey: 'echo_id', 
+  onDelete: 'CASCADE', 
+  onUpdate: 'CASCADE'
+});
 
 // Replies <> Echoes 
-Replies.belongsTo(Echoes, {foreignKey: 'echo_id'});
-Echoes.hasMany(Replies, {foreignKey: 'echo_id'});
+Replies.belongsTo(Echoes, {
+  foreignKey: 'echo_id', 
+  onDelete: 'CASCADE', 
+  onUpdate: 'CASCADE'
+});
+Echoes.hasMany(Replies, {
+  foreignKey: 'echo_id', 
+  onDelete: 'CASCADE', 
+  onUpdate: 'CASCADE'
+});
 
 // Replies <> User 
-Replies.belongsTo(User, {foreignKey: 'user_id'});
-User.hasMany(Replies, {foreignKey: 'user_id'});
+Replies.belongsTo(User, {
+  foreignKey: 'user_id', 
+  onDelete: 'CASCADE', 
+  onUpdate: 'CASCADE'
+});
+User.hasMany(Replies, {
+  foreignKey: 'user_id', 
+  onDelete: 'CASCADE', 
+  onUpdate: 'CASCADE'
+});
 
 // Replies <> Replies (nested replies)
-Replies.belongsTo(Replies, {foreignKey: 'parent_reply_id', as: 'parent_reply'});
-Replies.hasMany(Replies, {foreignKey: 'parent_reply_id', as: 'child_replies'});
+Replies.belongsTo(Replies, {
+  foreignKey: 'parent_reply_id', 
+  as: 'parent_reply', 
+  onDelete: 'CASCADE', 
+  onUpdate: 'CASCADE'
+});
+Replies.hasMany(Replies, {
+  foreignKey: 'parent_reply_id', 
+  as: 'child_replies', 
+  onDelete: 'CASCADE', 
+  onUpdate: 'CASCADE'
+});
 
 // Media <> Replies 
-Media.belongsTo(Replies, {foreignKey: 'reply_id'});
-Replies.hasMany(Media, {foreignKey: 'reply_id'});
+Media.belongsTo(Replies, {
+  foreignKey: 'reply_id', 
+  onDelete: 'CASCADE', 
+  onUpdate: 'CASCADE'
+});
+Replies.hasMany(Media, {
+  foreignKey: 'reply_id', 
+  onDelete: 'CASCADE', 
+  onUpdate: 'CASCADE'
+});
 
 // Reactions <> Echoes 
-Reactions.belongsTo(Echoes, {foreignKey: 'echo_id'});
-Echoes.hasMany(Reactions, {foreignKey: 'echo_id'});
+Reactions.belongsTo(Echoes, {
+  foreignKey: 'echo_id', 
+  onDelete: 'CASCADE', 
+  onUpdate: 'CASCADE'
+});
+Echoes.hasMany(Reactions, {
+  foreignKey: 'echo_id', 
+  onDelete: 'CASCADE', 
+  onUpdate: 'CASCADE'
+});
 
 // Reactions <> User 
-Reactions.belongsTo(User, {foreignKey: 'user_id'});
-User.hasMany(Reactions, {foreignKey: 'user_id'});
+Reactions.belongsTo(User, {
+  foreignKey: 'user_id', 
+  onDelete: 'CASCADE', 
+  onUpdate: 'CASCADE'
+});
+User.hasMany(Reactions, {
+  foreignKey: 'user_id', 
+  onDelete: 'CASCADE', 
+  onUpdate: 'CASCADE'
+});
 
 // Reports <> Echoes 
-Reports.belongsTo(Echoes, {foreignKey: 'echo_id'});
-Echoes.hasMany(Reports, {foreignKey: 'echo_id'});
+Reports.belongsTo(Echoes, {
+  foreignKey: 'echo_id', 
+  onDelete: 'CASCADE', 
+  onUpdate: 'CASCADE'
+});
+Echoes.hasMany(Reports, {
+  foreignKey: 'echo_id', 
+  onDelete: 'CASCADE', 
+  onUpdate: 'CASCADE'
+});
 
 // Reports <> Users
-Reports.belongsTo(User, {foreignKey: 'user_id', as: 'reporter'});
-User.hasMany(Reports, {foreignKey: 'user_id', as: 'submitted_reports'});
+Reports.belongsTo(User, {
+  foreignKey: 'user_id', 
+  as: 'reporter', 
+  onDelete: 'CASCADE',
+  onUpdate: 'CASCADE'
+});
+User.hasMany(Reports, {
+  foreignKey: 'user_id', 
+  as: 'submitted_reports', 
+  onDelete: 'CASCADE',
+  onUpdate: 'CASCADE'
+});
 
 // Echoes <> Echo_visibility (1:1)
-Echoes.hasOne(Echo_visibility, {foreignKey: 'echo_id'});
-Echo_visibility.belongsTo(Echoes, {foreignKey: 'echo_id'});
+Echoes.hasOne(Echo_visibility, {
+  foreignKey: 'echo_id', 
+  onDelete: 'CASCADE',
+  onUpdate: 'CASCADE'
+});
+Echo_visibility.belongsTo(Echoes, {
+  foreignKey: 'echo_id', 
+  onDelete: 'CASCADE',
+  onUpdate: 'CASCADE'
+});
 
 // Echoes <> Tags (many to many through echo_tags)
 Echoes.belongsToMany(Tags, {
   through: Echo_tags,
   foreignKey: 'echo_id',
   otherKey: 'tag_id',
-  as: 'tags'
+  as: 'tags', 
+  onDelete: 'CASCADE',
+  onUpdate: 'CASCADE'
 }); 
 Tags.belongsToMany(Echoes, {
   through: Echo_tags, 
   foreignKey: 'tag_id',
   otherKey: 'echo_id',
-  as: 'echoes'
+  as: 'echoes', 
+  onDelete: 'CASCADE',
+  onUpdate: 'CASCADE'
 });
 
 // Echoes <> users (recipients- many to many)
@@ -76,13 +174,17 @@ Echoes.belongsToMany(User, {
   through: Echo_recipients,
   foreignKey: 'echo_id',
   otherKey: 'recipient_id',
-  as: 'recipients'
+  as: 'recipients', 
+  onDelete: 'CASCADE',
+  onUpdate: 'CASCADE'
 });
 User.belongsToMany(Echoes, {
   through: Echo_recipients, 
   foreignKey: 'recipient_id',
   otherKey: 'echo_id',
-  as: 'received_echoes'
+  as: 'received_echoes', 
+  onDelete: 'CASCADE',
+  onUpdate: 'CASCADE'
 });
 
 // Users <> Users (friends)
@@ -90,9 +192,10 @@ User.belongsToMany(User, {
   through: Friends,
   as: 'friends',
   foreignKey: 'user_id',
-  otherKey: 'friend_id'
-})
-
+  otherKey: 'friend_id', 
+  onDelete: 'CASCADE',
+  onUpdate: 'CASCADE'
+});
 
 module.exports = {
   db,
