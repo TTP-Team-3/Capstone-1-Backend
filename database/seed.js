d470a6ff464d910737f74c991fa0d8f77d874b0b
Aiyanna <20997291+aarcher711@users.noreply.github.com>
2025-08-07 11:37:24 -0400

    updating passwords on this branch

--- database/seed.js
+++ database/seed.js
@@ -9,7 +9,7 @@ const seed = async () => {
     const users = await User.bulkCreate([
       { username: "jeramy", password_hash: User.hashPassword("123456"), email: "jeramy@gmail.com" },
       { username: "aiyanna", password_hash: User.hashPassword("456789"), email: "aiyanna@gmail.com" },
-      { username: "emmanuel", password_hash: User.hashPassword("789"), email: "emmanuel@gmail.com" },
+      { username: "emmanuel", password_hash: User.hashPassword("789101"), email: "emmanuel@gmail.com" },
       { username: "olivia", password_hash: User.hashPassword("101112"), email: "olivia@gmail.com" },
     ]);
 
