11ac418e3dca7a31a8b9be5acfd0283051d43a97
jeramyleon <jeramyleon@gmail.com>
2025-08-11 14:02:59 -0400

    updated echoes model, changed echoes seed data to reflect thosechanges, added friends route that fetches all users friends that are accepted

--- api/index.js
+++ api/index.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const testDbRouter = require("./test-db");
 const usersRouter = require("./users")
 const echoesRouter = require("./echoes");
+const friendsRouter = require("./friends");
 
 router.use("/test-db", testDbRouter);
 router.use("/users", usersRouter);
 router.use("/echoes", echoesRouter);
+router.use("/friends", friendsRouter);
 module.exports = router;
