33be068517f16429f8ca08e9fc1fb0f198049faa
EmmanuelR21 <ruiz.emmanuel135@gmail.com>
2025-08-13 12:12:55 -0400

    refactor: Prettier add trailing commas

--- auth/index.js
+++ auth/index.js
@@ -76,7 +76,7 @@ router.post("/auth0", async (req, res) => {
         email: user.email,
       },
       JWT_SECRET,
-      { expiresIn: "24h" }
+      { expiresIn: "24h" },
     );
 
     res.cookie("token", token, {
@@ -137,7 +137,7 @@ router.post("/signup", async (req, res) => {
         email: user.email,
       },
       JWT_SECRET,
-      { expiresIn: "24h" }
+      { expiresIn: "24h" },
     );
 
     res.cookie("token", token, {
@@ -188,7 +188,7 @@ router.post("/login", async (req, res) => {
         email: user.email,
       },
       JWT_SECRET,
-      { expiresIn: "24h" }
+      { expiresIn: "24h" },
     );
 
     res.cookie("token", token, {
