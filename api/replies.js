514800ba08f3dba22e6b02a0b701b77f910c4ec3
jeramyleon <jeramyleon@gmail.com>
2025-08-13 15:42:37 -0400

    Implemented POST route to post replies, only allowed by authenticated users

---
+++ api/replies.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const router = express.Router();
+const { Replies, Echoes } = require("../database");
+const { authenticateJWT } = require("../auth");
+const { Op } = require("sequelize");
+const { rawListeners } = require("../app");
+
+router.post("/echoes/:id/reply", authenticateJWT, async(req, res) => {
+    try {
+        const userId = req.user.id; 
+        const echoId = parseInt(req.params.id, 10);
+        const { parent_reply_id, message } = req.body;
+
+        // check that echo exists 
+        const echo = await Echoes.findByPk(echoId);
+        if (!echo) {
+            return res.status(404).json({error: "Echo not found."});
+        }
+
+        // message field required 
+        if (!message) {
+            return res.status(400).json({error: "Message is required."});
+        }
+
+        // if parent reply is provided, ensure it belongs to the same echo 
+        if (parent_reply_id) {
+            const parentReply = await Replies.findByPk(parent_reply_id);
+            if (!parentReply || parentReply.echo_id !== echoId) {
+                return res.status(400).json({error: "Invalid parent reply for this echo."});
+            }
+        }
+        
+        const newReply = await Replies.create({
+            echo_id: echoId,
+            user_id: userId,
+            parent_reply_id,
+            message 
+        });
+
+        res.status(201).json({
+            message: "Reply added successfully.",
+            newReply
+        });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ error: "Error posting reply."});
+    }
+});
+
+router.get("echoes/:id/replies", async (req, res) => {
+
+}); 
+
+router.delete("/echoes/:echo_id/replies/:reply_id", authenticateJWT, async (req, res) => {
+
+});
+
